62081681399d219fab0c704c935a67f8b0ea584e
cattgt <catalina.saavedra.aguilera@gmail.com>
2025-09-21 16:58:29 -0300

    Update clabcalendar.py

--- clabcalendar.py
+++ clabcalendar.py
@@ -2,24 +2,31 @@
 import datetime as dt
 import pytz
 import streamlit as st
+import io
+from googleapiclient.http import MediaIoBaseUpload
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-# ✅ Permisos necesarios para Google Calendar y Google Sheets
+# ✅ Permisos necesarios para Google Calendar , Google Sheets y Google Drive
 SCOPES = [
     "https://www.googleapis.com/auth/calendar",
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive"
 ]
 
-# ✅ ID del calendario compartido del laboratorio
+#  ID del calendario compartido del laboratorio
 CALENDAR_ID = '088ad85ef8fdcb937ac7b19749449e0b20f331220495d55f5952394af31f1f30@group.calendar.google.com'
 TIMEZONE = 'America/Santiago'
 
-# ✅ ID de la hoja de cálculo y nombre de la hoja
+#  ID de la hoja de cálculo y nombre de la hoja
 SPREADSHEET_ID = '15l1xB9SyZPFPsQPA9b_xLBAaZLvvcNWXc0K12QEtlo8'
 SHEET_NAME = 'C-LAB RESERVA'
+
+# ID de la carpeta de archivos 
+DRIVE_FOLDER_ID = '1j0bgnibm9RW4-bsceulsZxw_sCXNiGTA'
+DRIVE_NAME = 'C-LAB RESERVA'
+
 
 class GoogleCalendarManager:
     def __init__(self):
@@ -28,6 +35,7 @@ class GoogleCalendarManager:
         )
         self.calendar_service = build("calendar", "v3", credentials=self.creds)
         self.sheets_service = build("sheets", "v4", credentials=self.creds)
+        self.drive_service = build("drive", "v3", credentials=self.creds) 
 
     def list_upcoming_events(self, max_results=10):
         chile_tz = pytz.timezone(TIMEZONE)
@@ -87,4 +95,28 @@ class GoogleCalendarManager:
         except HttpError as error:
             st.error(f"❌ Error al registrar en Google Sheets: {error}")
             return None
+            
+            # --- NUEVO: subir archivo a Drive ---
+    def upload_file_to_drive(self, file, filename, folder_id=DRIVE_FOLDER_ID):
+        """
+        file: archivo subido por Streamlit (st.file_uploader)
+        filename: nombre del archivo
+        folder_id: ID de la carpeta donde guardar
+        """
+        try:
+            file_metadata = {
+                'name': filename,
+                'parents': [folder_id]
+            }
+            media = MediaIoBaseUpload(io.BytesIO(file.getvalue()), mimetype=file.type)
+            uploaded_file = self.drive_service.files().create(
+                body=file_metadata,
+                media_body=media,
+                fields='id, webViewLink'
+            ).execute()
+            return uploaded_file.get('webViewLink')  # link para abrir el archivo
+        except Exception as e:
+            st.error(f"❌ Error al subir archivo a Drive: {e}")
+            return None
+        
 
